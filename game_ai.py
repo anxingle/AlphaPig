b83a17f0d37b130c774ec8eb1854d19bf48291df
安兴乐 <anxingle@shensz.cn>
2018-07-16 21:59:03 +0800

    feat:  使用预对弈的数据，并增大一些数据工作

--- game_ai.py
+++ game_ai.py
@@ -76,7 +76,7 @@ class Game_AI(object):
         states, mcts_probs, current_players = [], [], []
         blank_move_list = [0,1,2,3,4,5,6,7,8,15,16,17,18,19,20,21,22,23,30,31,32,33,34,35,36,37,38,45,46,47,48,49,50,51,52,53,60,61,62,63,64,65,66,67,68,75,76,77,78,79,80,81,82,83,90,91,92,93,94,95,96,97,98]
         white_move_list = range(0, 103)
-        if random.random() < 0.15:
+        if random.random() < 0.09:
             while True:
                 move_blank = random.choice(blank_move_list)
                 # move_blank = blank_move_list[random.randint(0, len()-1)]
